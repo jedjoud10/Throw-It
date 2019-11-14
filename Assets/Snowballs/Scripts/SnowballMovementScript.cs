b390f93e89280fb04915a13015b39f668c6a1c95
Jedjoud10 <jribijed6@gmail.com>
2019-11-14 18:51:33 -0500

    Added charging snowball
    Added charging snowball mechanic

--- Assets/Snowballs/Scripts/SnowballMovementScript.cs
+++ Assets/Snowballs/Scripts/SnowballMovementScript.cs
@@ -7,20 +7,21 @@ public class SnowballMovementScript : MonoBehaviour
     [Header("Settings")]
     public float Size;//The size of the snowball from the start
     public float Speed;//How fast does the snowball gets pushed from the start
-    public float Damage;//The base damage the snowball can do
+    public int Damage;//The base damage the snowball can do
     private Rigidbody rigidBody;//The rigidbody of the snowball
     // Start is called before the first frame update
-    private void Start()
+    public void InitSnowball(float _Speed)//Multiply our base values by those arguments
     {
         #region Setup Settings
         rigidBody = GetComponent<Rigidbody>();//Sets the rigidbody to our own
-        rigidBody.AddForce(rigidBody.transform.forward * Speed);//Pushes the snowball in the direction it is currently heading
-        transform.localScale *= Size;//Multiplies the current scale of the snowball by the Size variable
+        rigidBody.AddForce(rigidBody.transform.forward * Speed * _Speed);//Pushes the snowball in the direction it is currently heading. Multiply the speed by the _speed argument so we can change how fast we can throw it in the SnowballThrowingScript.cs script
+        transform.localScale *= Size;//Multiplies the current scale of the snowball by the Size variable.
         #endregion
     }
     //When we hit an object (Ex. : Player, Snowman, Ground)
     private void OnCollisionEnter(Collision collision)
     {
+        Damage *= Mathf.RoundToInt(rigidBody.velocity.magnitude);//Take account velocity to damage, so if the snowball is fast, it does more damage
 
         //Enter collision code handling
 
--- Assets/Snowballs/Scripts/SnowballThrowingScript.cs
+++ Assets/Snowballs/Scripts/SnowballThrowingScript.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 //Snowball throwing for player
 public class SnowballThrowingScript : MonoBehaviour
 {
-    public GameObject Snowball;//The snowball that we are going to throw
+    public GameObject Snowball;//The snowball prefab that we are going to throw
     public Transform ThrowPoint;//The point where the snowball is throwed
+    private GameObject InstanceSnowball;//The instance of the Snowball var
+    private float ChargePercent;//How much did we charge the snowball ?
+    public float ChargeIncement;//How fast does the charge rises ?
+    public float ChargeTimeThreshold;//How much time before starting the charging of the snowball
+    private float ChargeTime;//Variable taking track of the time the user held the mouse button
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +21,25 @@ public class SnowballThrowingScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))//Dedect left mouse button click
+        if (Input.GetMouseButton(0))//Dedect if we holding the left mouse button
         {
-            ThrowSnowball();
+            ChargeTime += Time.deltaTime;//Add one second based on the delay between each frame
+            if (ChargeTime > ChargeTimeThreshold)//If we held the button long enough, then start charging
+            {
+                ChargePercent = ChargePercent + ChargeIncement * Time.deltaTime; //Charge the throw
+                ChargePercent = Mathf.Clamp(ChargePercent, 0, 1);//Clamp the value so we stay between a 0 - 1 range
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            ThrowSnowball();//Throw snowball
+            ChargePercent = 0;//Reset the charge since we threw the snowball
+            ChargeTime = 0;//Reset the time since we threw the snowball
         }
     }
     public void ThrowSnowball()//Throw snowball method
     {
-        Instantiate(Snowball, ThrowPoint.position, ThrowPoint.rotation);//Throw snowball
+        InstanceSnowball = Instantiate(Snowball, ThrowPoint.position, ThrowPoint.rotation);//Throw snowball and set that spawned snoball as our variable so we can call the InitSnowball method
+        InstanceSnowball.GetComponent<SnowballMovementScript>().InitSnowball(ChargePercent);//Init the snowball with taking account the charging
     }
 }
