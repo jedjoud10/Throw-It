754be4b24e03a5b1cd1101b087c36ca92030d940
Jedjoud10 <jribijed6@gmail.com>
2019-11-14 20:32:47 -0500

    Added bot straight script

---
+++ Assets/Bots/Scripts/Movement and Pathfinding/Basic Bot/BotStraightPathScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BotMovementScript))]
+//Moves the bot using the BotMovementScript.cs in a straight line to the endPoint
+public class BotStraightPathScript : MonoBehaviour
+{
+    public Transform endPoint;//The end position that we want to go
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("InitEndPoint", 1.0f);//Call after delay
+    }
+    //So what we are doing is we are getting the BotMovementScript of this bot, then we tell it that we want to go to the end position. We call it with an invoke and with a delay because the position is the bots position at start
+    private void InitEndPoint() 
+    {
+        GetComponent<BotMovementScript>().MoveToPosition(endPoint.position);
+    }
+}
