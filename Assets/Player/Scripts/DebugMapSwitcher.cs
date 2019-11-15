8d15e0ce05acbed0224ee8ab4ac7eb5788156a97
Jedjoud10 <jribijed6@gmail.com>
2019-11-14 21:20:52 -0500

    Create DebugMapSwitcher.cs

---
+++ Assets/Player/Scripts/DebugMapSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//Switches between the two maps using two keys
+public class DebugMapSwitcher : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))//Load map 1
+        {
+            SceneManager.LoadScene("Map1", LoadSceneMode.Single);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))//Load map 2
+        {
+            SceneManager.LoadScene("Map2", LoadSceneMode.Single);
+        }
+    }
+}
