90b32199c2fa41dc5a356fb0816120c690df142a
Jedjoud10 <jribijed6@gmail.com>
2019-12-19 22:01:51 -0500

    Added snowball randomizer
    Added snowball randomizer to randomizer snowball properities like size and speed

--- Assets/Snow-Related Mechanics/Snowballs/Scripts/SnowballMovementScript.cs
+++ Assets/Snow-Related Mechanics/Snowballs/Scripts/SnowballMovementScript.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 //Controls the behaviour of the snowball movement and collisions
+[RequireComponent(typeof(SnowballProperities))]
 public class SnowballMovementScript : MonoBehaviour
 {
-    [Header("Settings")]
-    public float Size;//The size of the snowball from the start
-    public float Speed;//How fast does the snowball gets pushed from the start
-    public int Damage;//The base damage the snowball can do
+    private int Damage;//The base damage the snowball can do
     private Rigidbody rigidBody;//The rigidbody of the snowball
     // Start is called before the first frame update
     public void InitSnowball(float _Speed)//Multiply our base values by those arguments
     {
+        #region Setup properities
+        SnowballProperities properities = GetComponent<SnowballProperities>();//Gets properities from script
+
+        properities.InitSnowball();//Init snowball properities
+        float Speed = properities.Speed;//Use one time float since we wont reuse this float later on
+        Damage = properities.Damage;
+
+        #endregion
         #region Setup Settings
         rigidBody = GetComponent<Rigidbody>();//Sets the rigidbody to our own
         rigidBody.AddForce(rigidBody.transform.forward * Speed * _Speed);//Pushes the snowball in the direction it is currently heading. Multiply the speed by the _speed argument so we can change how fast we can throw it in the SnowballThrowingScript.cs script
-        transform.localScale *= Size;//Multiplies the current scale of the snowball by the Size variable.
         #endregion
     }
     //When we hit an object (Ex. : Player, Snowman, Ground)
---
+++ Assets/Snow-Related Mechanics/Snowballs/Scripts/SnowballProperities.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Holds information for the snowball (ex : size, damage, speed) and might randomize them
+public class SnowballProperities : MonoBehaviour
+{
+    [Header("Properities")]
+    public float Speed;//Speed force applied at start
+    public float Size;//Size of snowball
+    public int Damage;//Damage applied to someone/something when it collides with the snowball
+    [Header("Randomness")]
+    [Range(0, 2)]
+    public float Speed_Randomnes;//How much randomness to apply to speed
+    [Range(0, 2)]
+    public float Size_Randomnes;//How much randomness to apply to speed
+    [Range(0, 2)]
+    public float Damage_Randomnes;//How much randomness to apply to speed
+    //Randomizes the values
+    private void RandomizeValues() 
+    {
+        Speed += Random.Range(-Speed_Randomnes, Speed_Randomnes) * Speed;
+        Size += Random.Range(-Size_Randomnes, Size_Randomnes) * Size;
+        //Round to int since damage is int
+        Damage += Mathf.RoundToInt(Random.Range(-Damage_Randomnes, Damage_Randomnes) * Damage);
+    }
+    //Init snowball
+    public void InitSnowball() //Called from other scripts to init some properities and change them in some way. Also calles other stuff other from properities
+    {
+        RandomizeValues();//Randomize snowball values
+        SetSnowballWorldProperities();
+    }
+    //Set snowball game values from variables (Ex : size for local size)
+    private void SetSnowballWorldProperities() 
+    {
+        transform.localScale = new Vector3(Size, Size, Size);//Set world scale with size variable
+    }
+}
