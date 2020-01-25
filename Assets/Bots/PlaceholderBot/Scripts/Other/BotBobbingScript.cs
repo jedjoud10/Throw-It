93dc92c355d801f55fde7bf1584bd900b79c90d2
Jedjoud10 <jribijed6@gmail.com>
2020-01-25 17:00:45 -0500

    Changed smoothness of bobbing script. Made new test map. Fixed some track script stuff and added track to scrapbot model

--- Assets/Bots/PlaceholderBot/Scripts/Other/BotBobbingScript.cs
+++ Assets/Bots/PlaceholderBot/Scripts/Other/BotBobbingScript.cs
@@ -7,20 +7,22 @@ public class BotBobbingScript : MonoBehaviour
     public float frequency;//Frequency for the bot motion
     public float amplitude;//How much up and down motion is there ?
     public bool applybobbing = true;//Should we apply bobbing ?
-    private const float smoothness = 0.9f;//Smoothness to apply when we stop bobbing
+    private const float smoothness = 0.8f;//Smoothness to apply when we stop bobbing
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (applybobbing)
+        if(amplitude > 0.002) transform.localPosition = new Vector3(0, (Mathf.Sin(frequency * Time.time) * amplitude) + amplitude, 0);//Apply bobbing
+        else transform.localPosition = Vector3.zero;//Set pos to zero if amplitude is too low. Saving performence by not calculating sin function
+
+        if (!applybobbing)
         {
-            transform.localPosition = new Vector3(0, (Mathf.Sin(frequency * Time.time) * amplitude) + amplitude, 0);//Apply bobbing
+            amplitude = Mathf.Lerp(amplitude, 0, smoothness * Time.deltaTime);//Smooth out amplitude to zero
         }
-        else transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, smoothness*Time.deltaTime);
     }
 }
--- Assets/Bots/PlaceholderBot/Scripts/Other/TrackScript.cs
+++ Assets/Bots/PlaceholderBot/Scripts/Other/TrackScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 //Script to handle rotation of track of bot
 public class TrackScript : MonoBehaviour
 {
-    public Vector3 offset;//Offset of start position of ray
+    public Transform offset;//Offset of start position of ray
+    public float distanceOffset;//Offset of distance hit
+    private Vector3 point;//The hit point
+    private const float Smoothness = 5f;//Smoothnes from last frame hitpoint to current
     private RaycastHit hit;
     // Start is called before the first frame update
     void Start()
@@ -15,10 +18,12 @@ public class TrackScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Physics.Raycast(transform.position + offset, Vector3.down * 1000, out hit)) //Raycast with offset to ground
+        if(Physics.Raycast(offset.position, Vector3.down * 1000, out hit)) //Raycast with offset to ground
         {
-            Debug.DrawRay(transform.position + offset, Vector3.down * hit.distance, Color.black);//Debug
-            transform.LookAt(hit.point);//Make track rotate to look in the correct direction
+            point.x = hit.point.x; point.z = hit.point.z;//Default point without smoothing
+            point.y = Mathf.Lerp(point.y, hit.point.y, Smoothness * Time.deltaTime);//Smooth y-axis point, so smoothing affects track rotation too
+            Debug.DrawLine(offset.position, point + Vector3.up * distanceOffset, Color.black);//Debug
+            transform.LookAt(point + Vector3.up * distanceOffset);//Make track rotate to look in the correct direction
         }
     }
 }
