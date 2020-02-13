81173dd8ecd61f9ae4d1b828537cde61eb1f812f
Jedjoud10 <jribijed6@gmail.com>
2020-02-13 15:50:42 -0500

    Added temperature relative to water

--- Assets/Player/Scripts/TemperatureScript.cs
+++ Assets/Player/Scripts/TemperatureScript.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Player temperature calculations
+using UnityEngine.UI;
+//Player temperature calculations and UI temeprature stuff
 public class TemperatureScript : MonoBehaviour
 {
     private float temperature;//Current body temperature
     private float outsideTemperature;//The current outside temperature
     public float heatDispersion;//How much the outside temperature influences our own body temperature
     private const float targetTemperature = 37.0f;//The temperature that we want to come close to
+    private const float minTemperature = 35.5f;//Minimum temperature before we start getting damage
     public float targetTemperatureSpeed;//The speed at how much were going to get at targetTemperature from current temperature
     private Transform crystal;//De crystale
     private const float crystalTemperature = 37.0f;//The temperature that the crystal emmits at 0 distance
+    private const float waterColdness = -50.0f;//How much temperature to remove from outsideTemperature when we are in water
+    public float waterLevel;//The water level
     public float maxCrystalDistanceFalloff;//Name is pretty self-explanatory
     private HeatEmmiterScript[] heatEmmiters;//Objects that emmit heat
-    // Start is called before the first frame update
+    public Text temperatureText;//The text for showing temperature
+   // Start is called before the first frame update
     void Start()
     {
         crystal = GameObject.FindGameObjectWithTag("Objective").transform;//Get crystal from tag
@@ -23,9 +28,10 @@ public class TemperatureScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        outsideTemperature = TemperatureCrystal() + TemperatureHeatEmmiters();//Calculate oustide temperature
+        outsideTemperature = TemperatureCrystal() + TemperatureHeatEmmiters() + TemperatureWater();//Calculate oustide temperature
         temperature = Mathf.Lerp(temperature, outsideTemperature, heatDispersion * Time.deltaTime);
         temperature = Mathf.Lerp(temperature, targetTemperature, targetTemperatureSpeed * Time.deltaTime);//Get closer to target temperature
+        temperatureText.text = "Temperature : " + temperature.ToString("F2");
     }
     //Temperature relative to distance to crystal
     private float TemperatureCrystal() 
@@ -40,9 +46,14 @@ public class TemperatureScript : MonoBehaviour
         for(int i = 0; i < heatEmmiters.Length; i++) 
         {
             playerDist = Vector3.Distance(transform.position, heatEmmiters[i].transform.position);
-            TempHeatEmmiters += heatEmmiters[i].maxEmmisionDistance - playerDist;
+            TempHeatEmmiters += heatEmmiters[i].maxEmmisionDistance - playerDist;//Add temperature of all close heating objects
         }
         return TempHeatEmmiters;
+    }
+    //Temperature relative to if we are in water
+    private float TemperatureWater() 
+    {
+        return Mathf.Min((waterLevel - transform.position.y) / waterLevel * waterColdness, 0);//Water coldness clamped between -Infinity to 0 so we cant get heat from water if we are above, only remove heat if we are below
     }
     //Check if there is any new heat emmiters
     public void CheckHeatEmmiters() 
