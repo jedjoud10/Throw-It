c080bca357609a30d2975a61ecd11ae8b6e34ca7
Jedjoud10 <jribijed6@gmail.com>
2020-05-19 10:59:18 -0400

    Updated ThrowIt, Version: TestVersion

--- Assets/Other/Scripts/SaverLoader.cs
+++ Assets/Other/Scripts/SaverLoader.cs
@@ -6,10 +6,10 @@ using System;
 //Loads and saves data in files
 public class SaverLoader
 {
-    static string datadir = Application.persistentDataPath;
+    static string persistentDir = Application.persistentDataPath;
     public static object Load(string file, object defaultValue, Type type) 
     {
-        if (!File.Exists(datadir + "/" + file)) 
+        if (!File.Exists(persistentDir + "/" + file)) 
         {
             //Uh ohhh...stinkyyyyy...stinkyyyy error...hahahaha   
             Debug.LogWarning("File : " + file + " does not exist !");
@@ -17,14 +17,36 @@ public class SaverLoader
             return defaultValue;
         }
         object obj = defaultValue;
-        obj = JsonUtility.FromJson(File.ReadAllText(datadir + "/" + file), type);
+        obj = JsonUtility.FromJson(File.ReadAllText(persistentDir + "/" + file), type);
         Save(file, obj);//Resave just in case file has missing data
         return obj;
     }
     public static void Save(string file, object data) 
     {
         string stringData = JsonUtility.ToJson(data, true);
-        File.WriteAllText(datadir + "/" + file, stringData);
+        File.WriteAllText(persistentDir + "/" + file, stringData);
     }
-    public static bool Exists(string file) { return File.Exists(datadir + "/" + file); }//If a file exists
+    public static bool Exists(string file) { return File.Exists(persistentDir + "/" + file); }//If a file exists
+
+    //Using custom path
+    public static object Load(string path, string file, object defaultValue, Type type)
+    {
+        if (!File.Exists(persistentDir + "/" + file))
+        {
+            //Uh ohhh...stinkyyyyy...stinkyyyy error...hahahaha   
+            Debug.LogWarning("File : " + file + " does not exist !");
+            Save(file, defaultValue);
+            return defaultValue;
+        }
+        object obj = defaultValue;
+        obj = JsonUtility.FromJson(File.ReadAllText(path + "/" + file), type);
+        Save(file, obj);//Resave just in case file has missing data
+        return obj;
+    }
+    public static void Save(string path, string file, object data)
+    {
+        string stringData = JsonUtility.ToJson(data, true);
+        File.WriteAllText(path + "/" + file, stringData);
+    }
+    public static bool Exists(string path, string file) { return File.Exists(path + "/" + file); }//If a file exists
 }
